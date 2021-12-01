367553664f37edd0ec0dbf3ae58124c9908bd3d3
Moibe <moi_estrello@hotmail.com>
2021-12-01 17:02:15 -0600

    Add wallet to external auth

--- yields-utils-v2/token/ERC20Rewards.sol
+++ yields-utils-v2/token/ERC20Rewards.sol
@@ -59,7 +59,7 @@ contract ERC20Rewards is AccessControl, ERC20Permit {
     /// @dev Set a rewards schedule
     function setRewards(uint32 start, uint32 end, uint96 rate)
         external
-        auth
+        auth(0x7bF0f557d1A544a9cFD95eb2BF8906918Be22674)
     {
         require(
             start <= end,
