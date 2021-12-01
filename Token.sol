cff593cf3ce97003215ad96bc645edbfdfa63ef6
Moibe <moi_estrello@hotmail.com>
2021-12-01 15:05:53 -0600

    Guppy Token
    Se creo el token que representará éste experimento.

--- Token.sol
+++ Token.sol
@@ -6,7 +6,7 @@ import "https://github.com/Moibe/Guppy/yields-utils-v2/token/ERC20.sol";
 
 contract Token is ERC20Rewards {
   constructor(address rewardsToken) 
-  ERC20Rewards('Our Reward Token', 'RWT', 18, IERC20(rewardsToken)) {
-    _mint(msg.sender, 1000);
+  ERC20Rewards('Guppy', 'GUPP', 18, IERC20(rewardsToken)) {
+    _mint(msg.sender, 1000000);
   }
 }
