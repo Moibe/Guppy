36b1b23d06921bf4c11134b465d3b89e8e3daf60
Moibe <moi_estrello@hotmail.com>
2021-12-01 16:03:29 -0600

    Corrección cantidad de tokens

--- Reward.sol
+++ Reward.sol
@@ -6,6 +6,6 @@ import "https://github.com/Moibe/Guppy/yields-utils-v2/token/ERC20.sol";
 contract Reward is ERC20 {
   constructor() 
   ERC20('Guppy', 'GUPP', 18) {
-    _mint(msg.sender, 1000);
+    _mint(msg.sender, 100000000000000000000000000); //equivalente a 100 millones de tokens.
   }
 }
--- guppy.sol
+++ guppy.sol
@@ -8,6 +8,6 @@ import "https://github.com/Moibe/Guppy/yields-utils-v2/token/ERC20.sol";
 contract Token is ERC20Rewards {
   constructor(address rewardsToken) 
   ERC20Rewards('Guppy', 'GUPP', 18, IERC20(rewardsToken)) {
-    _mint(msg.sender, 1000000);
+    _mint(msg.sender, 100000000000000000000000000); //equivalente a 100 millones de tokens.
   }
 }
